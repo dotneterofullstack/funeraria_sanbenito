89e73cc81c303e2047ef45b285a3056da46c2e68
Omar García <omar.garcia.developer@hotmail.com>
2016-08-30 22:20:49 -0600

    Inicio de funcionalidad de creación de servicios funerarios

---
+++ FunerariaDAO/Models/ServicioFunerario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funeraria.DAL.Models
+{
+    public class ServicioFunerario : IFilter
+    {
+        public int ID { get; set; }
+        public int IdPaquete { get; set; }
+        public int IdAsesor { get; set; }
+        public int IdCliente { get; set; }
+        public int IdDomicilioCobranza { get; set; }
+        public DateTime FechaSolicitud { get; set; }
+        public string NumeroContrato { get; set; }
+        public string NumeroSolicitud{ get; set; }
+        public decimal Costo { get; set; }
+    }
+}
